2264fddf848f4578aa43ca5440e0f097da56d99f
SamTravail <105202149+SamTravail@users.noreply.github.com>
2022-08-08 14:45:28 +0200

    suite problème merge
    modification du code suite merge de la branche feature/roles

--- admin/listingRole.php
+++ admin/listingRole.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
 // Importation des fonctions
 require('../inc/pdo.php');
 require('../inc/fonction.php');
 
 // Selection dans la BDD articles, et affichage par date décroissante
-$select_articles = "SELECT * FROM rloes ORDER BY created_at DESC";
+$select_roles = "SELECT * FROM roles ORDER BY id ASC";
 
 // préparation pour l'injection SQL
-$query = $pdo->prepare($select_articles);
+$query = $pdo->prepare($select_roles);
 
 // INJECTION SQL
 $query->execute();
 
 // Affiche le résultat
-$articles = $query->fetchAll();
+$roles = $query->fetchAll();
 ?>
 
 <?php
@@ -23,30 +23,26 @@ $articles = $query->fetchAll();
 include('inc/header-back.php'); ?>
 
 <!-- création tu tableau pour affichage des résultats -->
-<h1>Liste des articles</h1>
+<h1>Liste des roles</h1>
 <table>
-   <thead>
-    <tr>
-        <th>id</th>
-        <th>titre</th>
-        <th>description</th>
-        <th>Status</th>
-        <th>Editer</th>
-    </tr>
-   </thead>
- 
-    <!-- affichage des éléments récuppérés dans le tableau -->
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>titre</th>
+            <th>description</th>
+            <th>Editer</th>
+        </tr>
+    </thead>
+
+<!-- affichage des éléments récuppérés dans le tableau -->
     <tbody>
         <?php foreach ($roles as $role) { ?>
-        <tr>
-            <td><?=$role['id']?></td>
-            <td><?=$role['titre']?></td>
-            <td><?=$role['description']?></td>
-            <td><?=$role['status']?></td>
-            <td><a href="editPost.php?id=<?=$role['id']?>">Editer</a></td>
+         <tr>
+            <td><?= $role['id'] ?></td>
+            <td><?= $role['titre'] ?></td>
+            <td><?= $role['description'] ?></td>
+            <td><a href="editPost.php?id=<?= $role['id'] ?>">Editer</a></td>
         </tr>
         <?php } ?>
-        <a id="back2Top" title="Back to top" href="#">&#10148;</a>
     </tbody>
 </table>
-<?php include('inc/footer-back.php'); ?>
