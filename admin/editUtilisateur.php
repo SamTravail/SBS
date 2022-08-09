71c3cbd5ef6ca0ba1a349520d19d9e59c70bc3eb
SamTravail <105202149+SamTravail@users.noreply.github.com>
2022-08-09 17:02:42 +0200

    Update editUtilisateur.php
    modification utilisateur

--- admin/editUtilisateur.php
+++ admin/editUtilisateur.php
@@ -4,21 +4,23 @@ require('../functions/pdo.php');
 require('../includes/fonction.php');
 
  // Réccupération de l'ID
- if(!empty($_GET['id_utilisateur']) && ctype_digit($_GET['id_utilisateur'])) {
-    $id = $_GET['id_utilisateur'];
+ if(!empty($_GET['id']) && ctype_digit($_GET['id'])) {
+    $id = $_GET['id'];
+
 // function getId($id) {
     global $pdo;
-    $sql = "SELECT * FROM utilisateurs WHERE id_utilisateur = :id_utilisateur";
+    $sql = "SELECT * FROM utilisateurs WHERE id_utilisateur = :id";
     $query = $pdo->prepare($sql);
-    $query->bindValue(':id_utilisateur', $id, PDO::PARAM_INT);
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
     $query->execute();
     $user = $query->fetch();
-    
+
+
     if(empty($user)) {
-        die('404');
+        die('404 2');
      }
 } else {
- die('404');
+ die('404 1');
 }
 
 // Traitement PHP
@@ -37,11 +39,11 @@ if(!empty($_POST['submitted'])) {
 
 
     // Vérification des champs pour validation
-    $errors = validText($errors,$prenom,'prenom',3,100);
-    $errors = validText($errors,$nom,'nom',10,1000);
+    $errors = validText($errors,$prenom,'prenom',1,100);
+    $errors = validText($errors,$nom,'nom',1,100);
     $errors = validText($errors, $pseudo, 'pseudo',2,50);
     $errors = validText($errors, $email, 'email',5,20);
-    $errors = validText($errors, $id_utilisateur, 'id_utilisateur',5,20);
+    $errors = validText($errors, $id_utilisateur, 'id_utilisateur',0,20);
     
     // Si pas d'erreurs, alors :
     if(count($errors) === 0) {
@@ -71,48 +73,25 @@ include('../includes/header-back.php'); ?>
     <h1>Edition d'un Utilisateur</h1>
     <form action="" method="post" novalidate class="wrap2">
         <label for="prenom">Prénom</label>
-        <input type="text" name="prenom" id="prenom" value="<?= $prenom['prenom']; ?>">
+        <input type="text" name="prenom" id="prenom" value="<?= $user['prenom']; ?>">
         <span class="error"><?php if(!empty($errors['prenom'])) { echo $errors['prenom']; } ?></span>
 
         <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom" value="<?= $nom['nom']; ?>">
+        <input type="text" name="nom" id="nom" value="<?= $user['nom']; ?>">
         <span class="error"><?php if(!empty($errors['nom'])) { echo $errors['nom']; } ?></span>
 
         <label for="pseudo">Pseudo</label>
-        <input type="text" name="pseudo" id="pseudo" value="<?= $pseudo['pseudo']; ?>">
+        <input type="text" name="pseudo" id="pseudo" value="<?= $user['pseudo']; ?>">
         <span class="error"><?php if(!empty($errors['pseudo'])) { echo $errors['pseudo']; } ?></span>
 
         <label for="email">Email</label>
-        <input type="text" name="email" id="email" value="<?= $email['email']; ?>">
+        <input type="text" name="email" id="email" value="<?= $user['email']; ?>">
         <span class="error"><?php if(!empty($errors['email'])) { echo $errors['email']; } ?></span>
 
         <label for="id_utilisateur">id_utilisateur</label>
-        <input type="text" name="id_utilisateur" id="id_utilisateur" value="<?= $id_utilisateur['id_utilisateur']; ?>">
+        <input type="text" name="id_utilisateur" id="id_utilisateur" value="<?= $user['id_utilisateur']; ?>">
         <span class="error"><?php if(!empty($errors['id_utilisateur'])) { echo $errors['id_utilisateur']; } ?></span>
-        
-        <?php
-        $status = array(
-            'draft' => 'Brouillon',
-            'publish' => 'Publié'
-        );
 
-        ?>
-        <label for="status">Status</label>
-        <select name="status">
-            <option value=""> ---------- Merci d'indiquer le status de l'article.</option>
-            <?php foreach ($status as $key => $value) {
-                $selected = '';
-                if(!empty($_POST['status'])) {
-                    if($_POST['status'] == $key) {
-                        $selected = ' selected="selected"';
-                    }
-                }elseif($status['status'] == $key) {
-                    $selected = ' selected="selected"';
-                }
-                ?>
-                <option value="<?php echo $key; ?>"<?php echo $selected; ?>><?php echo $value; ?></option>
-            <?php } ?>
-        </select>
         <span class="error"><?php if(!empty($errors['status'])) { echo $errors['status']; } ?></span> 
         <input type="submit" name="submitted" value="Modifier l'utilisateur' !">
     </form>
